0b73bcb316c71c48d81eaecae3a071bdaff9d0d3
Alexey <example@gmail.com>
2020-10-21 21:02:03 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// alert('1) Способы взаимодействия с пользователем.');
+
+
+
+// const result = confirm('Второй сопособ: да или нет confirm');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?:', '18');
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Сколько вам лет?', '');
+// answers[1] = prompt('Где проживаете?', '');
+// answers[2] = confirm('Холост?');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'alex'
+
+// alert(`Привет, ${user}`)
+
+// let incr = 10,
+// 	decr = 10;
+
+// inrc++;
+// decr--;
+
+// console.log(++incr);
+// console.log(decr--);
+
+// console.log(5%2);
+
+// console.log(2+2 == 5);
+// console.log(2+2 == 4);
+
+// console.log('2' === 2);
+// console.log(2 === 2);
+
+// && -   
+// ||
+
+
+
+
+
