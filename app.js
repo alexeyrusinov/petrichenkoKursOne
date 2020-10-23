f65a371e2de1de52d52803333a9a15becf146031
Alexey <example@gmail.com>
2020-10-23 06:00:10 +0500

    Created a first structure

--- app.js
+++ app.js
@@ -44,7 +44,22 @@
 // && -   
 // ||
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
+const a = prompt('Один из полседних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцениваете его', ''),
+		c = prompt('Один из полседних просмотренных фильмов?', ''),
+		d = prompt('На сколько оцениваете его', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
+console.log(personalMovieDB);
